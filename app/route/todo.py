951d278a2145b292ae5f8cb5ad89049e341452a1
claudiajh <claudia.with.jh@gmail.com>
2017-09-05 04:39:34 +0900

    check & uncheck function added.

--- app/route/todo.py
+++ app/route/todo.py
@@ -1,8 +1,29 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
-from app import app
+from app import app, db
+from app.models.todos import TodoModel
 
 
-@app.route('/todo')
+@app.route('/todo', methods=['GET', 'POST'])
 def todo_index():
-    return render_template('todo.html')
+    todo_list = TodoModel.query.all()
+
+    if request.method == 'POST':
+        new_todo = TodoModel(title=request.form['title'])
+
+        db.session.add(new_todo)
+        db.session.commit()
+
+        return redirect(url_for('todo_index'))
+
+    return render_template('todo.html', todos=todo_list)
+
+
+@app.route('/todo/<todo_id>/update', methods=['POST'])
+def todo_update(todo_id):
+    update_todo = TodoModel.query.filter_by(id=todo_id).one()
+
+    update_todo.done = not update_todo.done
+    db.session.commit()
+
+    return redirect(url_for('todo_index'))
